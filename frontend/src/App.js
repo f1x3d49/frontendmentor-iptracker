0cfb418d0a8818add12136338b73273c3f5feed1
f1x3d49 <davidnicoara28@gmail.com>
2023-07-13 13:04:32 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="uppercase">hello</div>;
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        vdgray: "hsl(0, 0%, 17%)",
+        dgray: "hsl(0, 0%, 59%)",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
